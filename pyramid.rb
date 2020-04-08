081d17362fa9501d046f321042fb6710f3ce1abc
zouzou <bzouleka@gmail.com>
2020-04-08 08:58:58 +0200

    .

--- pyramid.rb
+++ pyramid.rb
@@ -2,7 +2,7 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 nbr = gets.to_i
 i = nbr
 puts "Voici la pyramide : " 
-while (1<<i<<50)
+while (1<<i<<25)
 	myPyramid = lambda{|rows|
 	 rows.times do |i|
 	 puts "#{(' '*(nbr-i))+'€'*(i+1)}"
